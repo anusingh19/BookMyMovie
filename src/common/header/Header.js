93a6ac84fe587204fe085bedaed755d4924c97f8
Anu Kumar <anukumarsingh2009@gmail.com>
2022-06-13 20:44:49 +0530

    push

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,40 +1,166 @@
 import React, { Fragment, useState } from "react";
 import {
   Button,
+  FormControl,
+  Input,
+  InputLabel,
   Tab,
   Tabs,
   TextField,
   Typography,
+  createTheme,
 } from "@material-ui/core";
 import Logo from "../../assets/logo.svg";
 import Modal from "react-modal";
 import { Link } from "react-router-dom";
 import TabPanel from "../tabPanel/TabPanel";
 import "./Header.css";
+import { makeStyles } from '@material-ui/core/styles';
 
 const Header = ({ bookShow, bookShowId }) => {
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignContent: "nowrap",
+      justifyContent: "center",
+    },
+    formControl: {
+      marginLeft: theme.spacing(8),
+      marginRight: theme.spacing(8),
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(1),
+      minWidth: 120,
+    },
+    button: {
+      marginTop: theme.spacing(4),
+      marginBottom: theme.spacing(1),
+      marginLeft: theme.spacing(8),
+      marginRight: theme.spacing(8),
+    },
+  }));
+
+  const classes = useStyles();
+
   const [loginOpen, setLoginOpen] = useState(false);
   const [value, setValue] = useState(0);
-  const [login, setLogin] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [success, setSuccess] = useState(false);
+
+  const [loginValues, setLoginValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    contactNo: "",
+    firstNameError: false,
+    lastNameError: false,
+    emailError: false,
+    passwordError: false,
+    contactNoError: false,
+    registered: false,
+  });
 
   const onLogin = () => {
     setLoginOpen(true);
   };
 
-  const onChange = (event, newValue) => {
+  const handleModalChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const loginForm = () => {
-    setLogin(false);
-    setLoginOpen(false);
+  const handleLoginChange = (prop) => (event) => {
+    setLoginValues({ ...loginValues, [prop]: event.target.value });
   };
 
-  const registerForm = () => {
-    setLogin(false);
-    setSuccess(true);
+  const handleChange = (prop) => (event) => {
+    setValues({
+      ...values,
+      registered: false,
+      [prop]: event.target.value,
+      [prop + "Error"]:
+        event.target.value === undefined ||
+          event.target.value.trim() === ""
+          ? true
+          : false,
+    });
+  };
+
+  const handleLoginSubmit = (event) => {
+    const tryLogin = async () => {
+      try {
+        let myHeaders = new Headers();
+
+        myHeaders.append("access-control-allow-origin", "*");
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Cache-Control", "no-cache");
+        myHeaders.append(
+          "Authorization",
+          "Basic " +
+          window.btoa(loginValues.username + ":" + loginValues.password)
+        );
+
+        let response = await fetch(
+          'http://localhost:8085/api/v1/auth/login',
+          {
+            method: "POST",
+            headers: myHeaders,
+          }
+        );
+        if (response.ok && loggedIn !== true) {
+          setLoginOpen(false);
+          setLoggedIn(true);
+        }
+      } catch (e) { console.log(e) }
+    };
+    tryLogin();
+  };
+
+  const handleSubmit = (event) => {
+    console.log(event);
+
+    const tryRegister = async () => {
+      setValues({
+        ...values,
+        registered: false,
+      });
+
+      try {
+        const params = {
+          email_address: values.email,
+          first_name: values.firstName,
+          last_name: values.lastName,
+          mobile_number: values.contactNo,
+          password: values.password,
+        };
+        let response = await fetch(
+          'http://localhost:8085/api/v1/signup',
+          {
+            "access-control-allow-origin": "*",
+            method: "POST",
+            headers: {
+              Accept: "application/json;charset=UTF-8",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(params)
+          }
+        );
+        if (response.ok) {
+          setValues({
+            ...values,
+            registered: true,
+          });
+          setLoginOpen(false);
+        }
+      } catch (e) { }
+    };
+    tryRegister();
   };
 
   return (
@@ -44,7 +170,22 @@ const Header = ({ bookShow, bookShowId }) => {
           <img className="logo" src={Logo} alt="logo" />
         </Link>
         <div className="button-group">
-          {login ? (
+          {bookShow ?
+            loggedIn ? (
+              <Link
+                to={"/bookshow" + bookShowId}
+                style={{ textDecoration: "none" }}
+              >
+                <Button variant="contained" name="Book Show" color="primary">
+                  Book Show
+                </Button>
+              </Link>
+            ) : (
+              <Button variant="contained" name="Book Show" color="primary" onClick={onLogin}>
+                Book Show
+              </Button>
+            ) : null}
+          {!loggedIn ? (
             <Button variant="contained" name="Login" onClick={onLogin}>
               Login
             </Button>
@@ -53,22 +194,12 @@ const Header = ({ bookShow, bookShowId }) => {
               variant="contained"
               name="Logout"
               onClick={() => {
-                setLogin(true);
+                setLoggedIn(false);
               }}
             >
               Logout
             </Button>
           )}
-          {bookShow ? (
-            <Link
-              to={"/book-show/" + bookShowId}
-              style={{ textDecoration: "none" }}
-            >
-              <Button variant="contained" name="Book Show" color="primary">
-                Book Show
-              </Button>
-            </Link>
-          ) : null}
         </div>
       </div>
       <Modal
@@ -86,10 +217,10 @@ const Header = ({ bookShow, bookShowId }) => {
           content: {
             position: "absolute",
             top: "20%",
-            left: "30%",
-            right: "30%", 
-            width: "30%",
+            left: "40%",
+            right: "40%",
             height: 'fit-content',
+            width: 'min-content',
             border: "1px solid #ccc",
             background: "#fff",
             overflow: "auto",
@@ -100,78 +231,122 @@ const Header = ({ bookShow, bookShowId }) => {
           },
         }}
       >
-        <Tabs value={value} onChange={onChange}>
+        <Tabs
+          value={value}
+          indicatorColor="secondary"
+          onChange={handleModalChange}
+          aria-label="Login or Register">
           <Tab label="Login" />
           <Tab label="Register" />
         </Tabs>
-        <TabPanel value={value} index={0}>
-          <TextField label="Username" required />
-          <br />
-          <br />
-          <TextField
-            label="Password"
-            required
-            type="password"
-            
-          />
-          <br />
-          <br />
-          <br />
-          <br />
-          <Button
-            variant="contained"
-            onClick={loginForm}
-            color="primary"
-          >
-            Login
-          </Button>
-        </TabPanel>
+        <div className={classes.root}>
+          <TabPanel value={value} index={0}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="username">Username *</InputLabel>
+              <Input
+                id="username"
+                value={loginValues.username}
+                onChange={handleLoginChange("username")}
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="password">Password *</InputLabel>
+              <Input
+                id="password"
+                value={loginValues.password}
+                type="password"
+                onChange={handleLoginChange("password")}
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={handleLoginSubmit}
+              >
+                Login
+              </Button>
+            </FormControl>
+          </TabPanel>
+        </div>
         <TabPanel value={value} index={1}>
-          <TextField
-            label="First Name"
-            required
-          />
-          <br />
-          <br />
-          <TextField label="Last Name" required />
-          <br />
-          <br />
-          <TextField label="Email" required />
-          <br />
-          <br />
-          <TextField
-            required
-            label="Password"
-            type="password"
-          />
-          <br />
-          <br />
-          <TextField
-            required
-            label="Contact No."
-          />
-          <br />
-          <br />
-          {success ? (
-            <Fragment>
-              <Typography variant="subtitle1" gutterBottom>
-                Registration Successful. Please login!
+          <div className={classes.root}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="firstName">First Name *</InputLabel>
+              <Input
+                id="firstName"
+                value={values.firstName}
+                onChange={handleChange("firstName")}
+              />
+              <div style={{ color: "red", fontSize: "0.75rem" }}>
+                {values.firstNameError ? "required" : ""}
+              </div>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="lastName">Last Name *</InputLabel>
+              <Input
+                id="lastName"
+                value={values.lastName}
+                onChange={handleChange("lastName")}
+              />
+              <div style={{ color: "red", fontSize: "0.75rem" }}>
+                {values.lastNameError ? "required" : ""}
+              </div>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="email">Email *</InputLabel>
+              <Input
+                id="email"
+                value={values.email}
+                onChange={handleChange("email")}
+              />
+              <div style={{ color: "red", fontSize: "0.75rem" }}>
+                {values.emailError ? "required" : ""}
+              </div>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="password">Password *</InputLabel>
+              <Input
+                id="password"
+                value={values.password}
+                type="password"
+                onChange={handleChange("password")}
+              />
+              <div style={{ color: "red", fontSize: "0.75rem" }}>
+                {values.passwordError ? "required" : ""}
+              </div>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="contactNo">Contact No *</InputLabel>
+              <Input
+                id="contactNo"
+                value={values.contactNo}
+                onChange={handleChange("contactNo")}
+              />
+              <div style={{ color: "red", fontSize: "0.75rem" }}>
+                {values.contactNoError ? "required" : ""}
+              </div>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <Typography>
+                {values.registered
+                  ? "Registration Successful. Please Login!"
+                  : ""}
               </Typography>
-              <br />
-              <br />
-            </Fragment>
-          ) : null}
-          
-          <Button
-            variant="contained"
-            onClick={registerForm}
-            color="primary"
-          >
-            Register
-          </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={handleSubmit}
+              >
+                Register
+              </Button>
+            </FormControl>
+          </div>
         </TabPanel>
       </Modal>
-    </Fragment>
+    </Fragment >
   );
 };
 
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 import Header from '../../common/header/Header';
 import Heading from '../../common/heading/Heading';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
@@ -19,81 +19,179 @@ import {
   Checkbox,
   Button,
   createTheme,
+  ListItemText,
+  Typography,
 } from '@material-ui/core';
 import ReleasedMovies from './ReleasedMovies';
 import './Home.css';
 
-const useStyles = makeStyles({
-  grid: {
-    flexWrap: 'nowrap',
-    transform: 'translateZ(0)',
-  },
-});
-
-const theme = createTheme();
-
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [genres, setGenres] = useState([]);
-  const [artists, setArtists] = useState([]);
-  const [moviename, setMoviename] = useState('');
-  const [selectedGenres, setSelectedGenres] = useState([]);
-  const [selectedArtist, setSelectedArtist] = useState([]);
-  const [releaseStart, setReleaseStart] = useState(null);
-  const [releaseEnd, setReleaseEnd] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get('http://localhost:8085/api/v1/movies')
-      .then(response => setMovies(response.data.movies));
-
-    axios
-      .get('http://localhost:8085/api/v1/genres')
-      .then(response => setGenres(response.data.genres));
-
-    axios
-      .get('http://localhost:8085/api/v1/artists')
-      .then(response => setArtists(response.data.artists));
-  }, []);
-
-  const onMovieNameChange = (event) => {
-    setMoviename(event.target.value);
-  };
-
-  const onGenreChange = event => {
-    const {
-      target: { value },
-    } = event;
-    setSelectedGenres(
-      typeof value === 'string' ? value.split(',') : value,
-    );
-  };
-
-  const onArtistChange = event => {
-    const {
-      target: { value },
-    } = event;
-    setSelectedArtist(
-      typeof value === 'string' ? value.split(',') : value,
-    );
-  };
-
-  const onStartDateChange = event => {
-    setReleaseStart(event.target.value);
-  };
-
-  const onEndDateChange = event => {
-    setReleaseEnd(event.target.value);
-  }
+  const useStyles = makeStyles((theme) => ({
+    grid: {
+      flexWrap: 'nowrap',
+      transform: 'translateZ(0)',
+    },
+    root: {
+      float: "right",
+      margin: theme.spacing(1, 'auto'),
+      minWidth: 240,
+      maxWidth: 240,
+    },
+    title: {
+      color: theme.palette.primary.light,
+    },
+    withMargin: {
+      marginBottom: theme.spacing(1, 'auto'),
+      marginTop: theme.spacing(1, 'auto')
+    },
+    button: {
+      width: "100%"
+    }
+  }));
 
   const classes = useStyles();
+
+  const [allMovies, setAllMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [filter, setFilter] = useState(null);
+  const [values, setValues] = useState({
+    movieName: "",
+    genre: [],
+    artist: [],
+    from: "",
+    to: "",
+  });
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        let response = await fetch(
+          'http://localhost:8085/api/v1/movies?page=1&limit=100000'
+        );
+        let result = await response.json();
+        setAllMovies(result.movies);
+        response = await fetch('http://localhost:8085/api/v1/genres');
+        result = await response.json();
+        setGenres(result.genres.map((element) => element.description));
+        response = await fetch('http://localhost:8085/api/v1/artists');
+        result = await response.json();
+        setArtists(
+          result.artists.map(
+            (element) =>
+              element.first_name + " " + element.last_name
+          )
+        );
+      } catch (_) { }
+    };
+    getMovies();
+  }, []);
+
+  const releasedMoviesAll = allMovies.filter(
+    (movie) => movie.status.toLowerCase() === "released"
+  ) || [];
+
+  const filterHasArtists = (artists, movie) => {
+    const makeFullName = (first, last) =>
+      (first + " " + last).toLowerCase();
+
+    let found = false;
+    artists.forEach((artist) => {
+      if (
+        movie.artists.find((element) =>
+          makeFullName(element).includes(makeFullName(artist))
+        ) !== undefined
+      )
+        found = true;
+    });
+
+    return found;
+  };
+
+  const filterHasGenre = (genres, movie) => {
+    let found = false;
+    genres.forEach((genre) => {
+      if (
+        movie.genres.find((element) =>
+          element.toLowerCase().includes(genre.toLowerCase())
+        ) !== undefined
+      )
+        found = true;
+    });
+
+    return found;
+  };
+
+  // assuming the format from database is still YYYY-MM-DD
+  const dateInRange = (dateCheck, dateFrom, dateTo) => {
+    let date = new Date(dateCheck);
+
+    if (
+      dateFrom !== undefined &&
+      dateFrom !== null &&
+      dateFrom.toLowerCase().trim() !== ""
+    ) {
+      let from = new Date(dateFrom);
+      if (date < from) return false;
+    }
+
+    if (
+      dateTo !== undefined &&
+      dateTo !== null &&
+      dateTo.toLowerCase().trim() !== ""
+    ) {
+      let to = new Date(dateTo);
+      if (date > to) return false;
+    }
+
+    return true;
+  };
+
+  const releasedMovies =
+    filter === null
+      ? releasedMoviesAll
+      : releasedMoviesAll.filter(
+        (movie) =>
+          (filter.movieName === null ||
+            filter.movieName.trim() === "" ||
+            movie.title
+              .toLowerCase()
+              .includes(filter.movieName)) &&
+          (filter.artist === null ||
+            filter.artist.length === 0 ||
+            filterHasArtists(filter.artist, movie)) &&
+          (filter.genre === null ||
+            filter.genre.length === 0 ||
+            filterHasGenre(filter.genre, movie)) &&
+          dateInRange(movie.release_date, filter.from, filter.to)
+      );
+
+  const onFilterCallback = () => {
+    setFilter(values);
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 11.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
 
   return (
     <Fragment>
       <Header bookShow={false} />
       <Heading />
       <ImageList className={classes.grid} cols={6} rowHeight={250}>
-        {movies.map(tile => (
+        {allMovies.map(tile => (
           <ImageListItem key={tile.id}>
             <img src={tile.poster_url} alt={tile.title} />
             <ImageListItemBar
@@ -105,95 +203,98 @@ const Home = () => {
       </ImageList>
       <div className='second'>
         <div className='released'>
-          <ReleasedMovies movies={movies} />
+          <ReleasedMovies movies={releasedMovies} />
         </div>
         <div className='filter'>
           <div className='cardComponent'>
-            <Card>
+            <Card className={classes.root}>
               <CardContent>
-                <InputLabel style={{ color: theme.palette.primary.light }}>
+                <Typography gutterBottom component="h3" className={classes.title}>
                   FIND MOVIES BY:
-                </InputLabel>
-                <FormControl variant="standard" style={{ width: '100%', margin: theme.spacing(1, 'auto') }}>
-                  <InputLabel htmlFor="component-simple">Movie Name</InputLabel>
-                  <Input id="component-simple" value={moviename} onChange={onMovieNameChange} />
+                </Typography>
+
+                <FormControl fullWidth className={classes.withMargin}>
+                  <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                  <Input
+                    id="movieName"
+                    value={values.movieName}
+                    onChange={handleChange("movieName")}
+                  />
                 </FormControl>
-                <FormControl variant="standard" style={{ width: '100%', margin: theme.spacing(1, 'auto') }}>
-                  <InputLabel htmlFor="component-simple">Genres</InputLabel>
+
+                <FormControl fullWidth className={classes.withMargin}>
+                  <InputLabel id="genres-label">Genres</InputLabel>
                   <Select
-                    labelId='demo-mutiple-name-label'
-                    id='demo-mutiple-name'
+                    labelId="genres-label"
+                    id="genres"
                     multiple
-                    value={selectedGenres}
-                    onChange={onGenreChange}
-                    input={<Input label="Genres" />}
-                  >
-                    {genres.map(genre => (
-                      <MenuItem key={genre.id} value={genre.genre}>
-                        <Checkbox color='primary' />
-                        {genre.genre}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-                <FormControl
-                  style={{ width: '100%', margin: theme.spacing(1, 'auto') }}
-                >
-                  <InputLabel id='demo-mutiple-name-label'>Artists</InputLabel>
-                  <Select
-                    labelId='demo-mutiple-name-label'
-                    id='demo-mutiple-name'
-                    multiple
-                    value={selectedArtist}
-                    onChange={onArtistChange}
+                    value={values.genre}
+                    onChange={handleChange("genre")}
                     input={<Input />}
+                    renderValue={(selected) => selected.join(", ")}
+                    MenuProps={MenuProps}
                   >
-                    {artists.map(artist => (
-                      <MenuItem
-                        key={artist.id}
-                        value={artist.first_name + ' ' + artist.last_name}
-                      >
-                        <Checkbox color='primary' />
-                        {artist.first_name + ' ' + artist.last_name}
+                    {genres.map((element, index) => (
+                      <MenuItem value={element} key={index}>
+                        <Checkbox checked={values.genre.indexOf(element) > -1} />
+                        <ListItemText primary={element} />
                       </MenuItem>
                     ))}
                   </Select>
                 </FormControl>
-                <FormControl
-                  style={{ width: '100%', margin: theme.spacing(1, 'auto') }}
-                >
-                  <TextField
-                    name='Release Date Start'
-                    id='standard-basic'
-                    type='date'
-                    label='Release Date Start'
-                    value={releaseStart}
-                    onChange={onStartDateChange}
-                    InputLabelProps={{ shrink: true }}
-                  />
-                </FormControl>
-                <FormControl
-                  style={{ width: '100%', margin: theme.spacing(1, 'auto') }}
-                >
-                  <TextField
-                    name='Release Date End'
-                    id='standard-basic'
-                    type='date'
-                    label='Release Date End'
-                    value={releaseEnd}
-                    onChange={onEndDateChange}
-                    InputLabelProps={{ shrink: true }}
-                  />
-                </FormControl>
-                <div>
-                  <FormControl
-                    style={{ width: '100%', margin: theme.spacing(1, 'auto') }}
+
+                <FormControl fullWidth className={classes.withMargin}>
+                  <InputLabel id="artist-label">Artists</InputLabel>
+                  <Select
+                    labelId="artist-label"
+                    id="artist"
+                    multiple
+                    value={values.artist}
+                    onChange={handleChange("artist")}
+                    input={<Input />}
+                    renderValue={(selected) => selected.join(", ")}
+                    MenuProps={MenuProps}
                   >
-                    <Button variant='contained' name='Apply' color='primary'>
-                      Apply
-                    </Button>
-                  </FormControl>
-                </div>
+                    {artists.map((element, index) => (
+                      <MenuItem value={element} key={index}>
+                        <Checkbox checked={values.artist.indexOf(element) > -1} />
+                        <ListItemText primary={element} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+
+                <FormControl fullWidth className={classes.withMargin}>
+                  <TextField
+                    id="startDate"
+                    label="Release Date Start"
+                    type="date"
+                    defaultValue=""
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    onChange={handleChange("from")}
+                  />
+                </FormControl>
+                <FormControl fullWidth className={classes.withMargin}>
+                  <TextField
+                    id="endDate"
+                    label="Release Date End"
+                    type="date"
+                    defaultValue=""
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    onChange={handleChange("to")}
+                    format="DD-MM-YYYY"
+                  />
+                </FormControl>
+                <br />
+                <br />
+                <br />
+                <Button variant="contained" color="primary" disableElevation onClick={onFilterCallback}>
+                  APPLY
+                </Button>
               </CardContent>
             </Card>
           </div>
